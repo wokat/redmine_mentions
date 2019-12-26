c41aa1c56a9b6c148f177c280b638877901c3a30
TAINE <taine@live.com>
2019-12-26 21:29:21 +0800

    bump version v0.1.1

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_mentions do
   name 'Redmine Mentions'
   author 'Arkhitech, Taine Woo'
   description 'This is a plugin for Redmine which gives suggestions on using username in comments'
-  version '0.1.0'
+  version '0.1.1'
   url 'https://github.com/tainewoo/redmine_mentions'
   author_url 'https://github.com/tainewoo'
   settings :default => {'trigger' => '@'}, :partial => 'settings/mention'
